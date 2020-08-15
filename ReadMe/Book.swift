d4cfda32003269d713e352d16bfc5a3023ac8a21
andersonchagas <anderson.1997reis@gmail.com>
2020-08-15 20:10:12 -0300

    configurando table view

---
+++ ReadMe/Book.swift
@@ -0,0 +1,17 @@
+//
+//  Book.swift
+//  ReadMe
+//
+//  Created by Anderson Chagas on 15/08/20.
+//  Copyright © 2020 Anderson Chagas. All rights reserved.
+//
+
+import UIKit
+
+struct Book {
+    let title : String
+    let author : String
+    var image : UIImage {
+        LibrarySymbol.letterSquare(letter: title.first).image
+    }
+}
